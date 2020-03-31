61b956cc7fc853a32cdeb22cd032e85c5226b7d8
Animationkgs <62798552+Animationkgs@users.noreply.github.com>
2020-03-31 23:54:57 +0530

    Add files via upload

---
+++ srt003meaning.js
@@ -0,0 +1,11 @@
+
+
+var srt003meaning= `
+3 - Measure Memory - lang_en_vs4.srt
+To get a good grade, we need a good memory.
+Search for factors that influence memory.
+Can't analyze the relationship between these and memory without a clear definition and way of measuring memory.
+How would you measure memory?
+`;
+
+
