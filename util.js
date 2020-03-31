8dbd98721fc4fee4c29b58e9796da79c5e6b721d
Animationkgs <62798552+Animationkgs@users.noreply.github.com>
2020-03-31 21:21:14 +0530

    Add files via upload

--- util.js
+++ util.js
@@ -37,7 +37,7 @@ var html= {
    richdiv :  function (id,s) { return ` <div id="${id}"> ${s} </div> `; }, 
    anonymousdiv : function (x) { return ` <div> ${x} </div> `; }, 
    iframe : function (vid) {
-	return ` <iframe width="440" height="248" src="https://www.youtube.com/embed/${vid}?rel=0&amp;autoplay=1&mute=1&cc_load_policy=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> `;
+	return ` <iframe width="440" height="248" src="https://www.youtube.com/embed/${vid}?rel=0&amp;mute=1&cc_load_policy=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> `;
    },
    richbutton : function (id, code, s) {
 	return ` <button id= ${id} onclick= "(${code})()"> ${s} </button> `;
