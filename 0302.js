0036313778377a8ce55f9342f336799817fe37bd
Animationkgs <62798552+Animationkgs@users.noreply.github.com>
2020-03-31 23:50:08 +0530

    Add files via upload

---
+++ 0302.js
@@ -0,0 +1,41 @@
+
+
+function intro0302() {
+  var ret= new Object();
+
+  ret.url= 'https://classroom.udacity.com/courses/st095/lessons/55856454/concepts/595961680923';
+  ret.vid= 'TSm4D-YHvzM';
+  ret.subtitlesfile= '3 - Measure Memory - lang_en_vs4.srt';
+
+  return ret;
+}
+
+
+function quiz0302() {
+  var ret= new Object();
+
+  ret.image= '0302.jpg';
+  ret.ocr= `Memory A+
+Factors that influence memory.
+
+But first!
+How would you measure memory?`;
+
+  ret.solution= `Measure memory with a memory game.`;
+  ret.solutionfeedback= `
+Thanks for completing that!
+Thanks for thinking about this!
+  `;
+  return ret;
+}
+
+
+function lesson0302() {
+	var ret= new Object();
+  ret.intro= intro0302();
+  ret.quiz= quiz0302();
+  ret.url= 'https://classroom.udacity.com/courses/st095/lessons/55856454/concepts/595961680923';
+
+  return ret;
+}
+
